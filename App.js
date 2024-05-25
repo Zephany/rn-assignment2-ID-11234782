329182666a6816d2ba52a13781da3810077acb1d
Zephany <zephanilaopoku4@gmail.com>
2024-05-25 09:36:37 +0000

    modifed App

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import {StyleSheet,Text,view} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return (
-        <View style={StyleSheet.container}>
-            <Text styles={styles.text}>
-                My name is <Text style={styles.boldText}>Zephanila</Text>
-            </Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.boldText}>Zephanila</Text>
+      </Text>
+    </View>
+  );
 }
 
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'blue',
-        alignItems: 'center',
-        justifyContent:'center',
-    },
-    text:{
-        fontSize:24,
-    },
-    boldText:{
-        fontWeight:'bold'
-    }
-}
-)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ADD8E6', // Change background color
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24, // Increase font size
+  },
+  boldText: {
+    fontWeight: 'bold', // Make name bold
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # rn-assignment2-ID-11234782
+This is a simple React Native application created using Expo Cli.The application displays a message with the user's name in bold.
+## Task Description
+-changed the background color of the 'View'component.
+-displayed the text "My name is Zephanila".
+-increased the font size to 25.
+-made the 'YourName'bold.
+## Screenshot
+[Screenshot of the application](/assets/screenshot.jpg)
+
+## Student ID
+11234782
